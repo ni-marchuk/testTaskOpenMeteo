4a78c29e46af19cc314f79b66e0aa389bab53553
marchuk <marchuk@ddplanet.ru>
2025-07-15 14:22:55 +0300

    Updates

--- src/app/providers/TanstackReactQueryProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { type FC, type PropsWithChildren } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
-export const TanstackReactQueryProvider: FC<PropsWithChildren> = ({ children }) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-};
--- src/shared/types/types.ts
+++
@@ -1,9 +0,0 @@
-export type Coordinates = {
-  latitude: number;
-  longitude: number;
-};
-
-export type ChartColors = {
-  border: string;
-  bg: string;
-};
